7c9b9aa5ab8c1363e2b9fb8ef2ccc0f59fa4f18b
Ravi Sharma <rs5957636@gmail.com>
2025-06-24 18:37:51 +0530

    Updated script.js and index.ejs

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}))
 
+
 app.get("/",function(req,res){
-  res.render("index");
+  res.render("index")
 
 })
-
-
-app.get("/check", function(req,res){
-    res.send("working");
-    console.log(req.query);
+app.post("/create",function(req,res){
+ 
+  res.send("workings propely and you are properly")
+  console.log(req.body);
 })
 app.listen(3000, () => {
   console.log("server is running at http://localhost:3000");
